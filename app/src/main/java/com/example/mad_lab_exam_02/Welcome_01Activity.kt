187643eebf1c306d25e1a3800f14c8bc01f1ff28
Scheffel333 <118644309+Scheffel333@users.noreply.github.com>
2024-03-08 10:08:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_lab_exam_02/Welcome_01Activity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_lab_exam_02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Welcome_01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome01)
+    }
+}
